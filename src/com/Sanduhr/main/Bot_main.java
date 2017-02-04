8c60435206e1070df2f5c5cab88b486fe2e26bc2
Sanduhr32 <Maurice.Craft@t-online.de>
2017-02-04 21:55:13 +0100

    Add files via upload

--- src/com/Sanduhr/main/Bot_main.java
+++ src/com/Sanduhr/main/Bot_main.java
@@ -7,27 +7,22 @@ import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.entities.Guild;
 
-import java.nio.charset.Charset;
 import java.util.List;
-import java.nio.file.*;
-import static java.nio.file.StandardOpenOption.*;
-import java.io.*;
 
 public class Bot_main {
     private static JDA JDA;
-    public static String BOT_TOKEN = ""; //Insert your Application Token
-    public static final String BOT_GAME = "created by Sanduhr.exe"; //DONT CHANGE THE DEFAULT GAME ORE GIVE ME CREDIT SOMEWHERE ELSE
+    public static final String BOT_TOKEN = ""; //Insert your Application Token
+    public static final String BOT_GAME = "empowered by Sanduhr.exe"; //DONT CHANGE THE DEFAULT GAME OR GIVE ME CREDIT SOMEWHERE ELSE
     public static void main(String[] args)throws Exception {
 
         JDABuilder j = new JDABuilder(AccountType.BOT);
-        j.setToken(t);
+        j.setToken(BOT_TOKEN);
         j.setGame(Game.of(BOT_GAME));
         j.setStatus(OnlineStatus.ONLINE);
         j.addListener(new Command_pub());
         j.addListener(new Command_guildowner());
         j.addListener(new Command_whitelisted());
         j.addListener(new Guildevent());
-        j.addListener(new Command_priv());
         JDA jda = j.buildBlocking();
         JDA = jda;
         System.out.println(jda.getAccountType());
