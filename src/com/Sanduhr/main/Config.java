581a4b9511a7596b4bc596647275cfcfd4e0ca6d
Sanduh32 <Maurice.Craft@t-online.de>
2017-02-12 07:16:19 +0100

    CLEARING 2.0 soonTM

--- src/com/Sanduhr/main/Config.java
+++
@@ -1,9 +0,0 @@
-package com.Sanduhr.main;
-
-/**
- * Created by Sanduhr on 08.02.2017.
- */
-public class config {
-    public static final String BOT_TOKEN = "";
-    public static final String BOT_GAME = "empowered by Sanduhr.exe";
-}
