0dcd059fea42e57c6ae8aed74f3f8010c49b692d
Sanduhr32 <Maurice.Craft@t-online.de>
2017-02-14 13:46:12 +0100

    Update Time.java
    |Mobile|IDE|
    |[CODE][1]|[CODE][2]|
    
    [1]: <https://hastebin.com/ovuhulafuc.java>
    [2]: <https://hastebin.com/evurasuter.java>

--- src/com/sanduhr/main/commands/pub/Time.java
+++ src/com/sanduhr/main/commands/pub/Time.java
@@ -1,38 +1,60 @@
 package com.sanduhr.main.commands.pub;
+
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.MessageUpdateEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.sanduhr.main.Lib.*;
+
 public class Time extends ListenerAdapter {
+
     @Override
-    public void onMessageReceived(MessageReceiveEvent e) {
-        //Never respond to a bot
-        if (e.getAurhor().isBot()) {
-            return;
-        }
+    public void onMessageReceived(MessageReceivedEvent e) {
         String content = e.getMessage().getContent();
-        //If the commamd isn't time
-        if (!content.equalsIgnoreCase(Lib.PREFIX + "time")) {
+
+        //Never respond to a bot!
+        if (e.getAuthor().isBot())
+            return;
+
+        //Not the `Time` command
+        if (!content.equalsIgnoreCase(PREFIX + "Time")) {
             return;
         }
-        Lib.receivedcmd++;
+
+        receivedcmd++;
+
+        DateTimeFormatter DTF = DateTimeFormatter.ofPattern("EE dd.MM.YYYY, HH:mm:ss | KK:mm:ss a");
         OffsetDateTime now = OffsetDateTime.now();
-        DateTimeFormatter DTF = DateTimeFormatter.ofPattern("");
-        e.getChannel.sendMessage(now.format(DTF)).queue();
+        e.getChannel().sendMessage("It's " + now.format(DTF)).queue();
+        executedcmd++;
     }
-    publix void onMessageUpdate(MessageUpdateEvent e) {
-        onMessageReceived(new MessageReceivedEvent(e.getJDA, e.getReponseNumber, e.getMessage));
+
+    public void onMessageUpdate(MessageUpdateEvent e) {
+        onMessageReceived(new MessageReceivedEvent(e.getJDA(), e.getResponseNumber(), e.getMessage()));
     }
+
     public void onReady(ReadyEvent e) {
         initter();
     }
+
     public void initter() {
-        Lib.getCmdMap().put(getName(), getDescription());
-        Lib.getSynMap().put(getName(), getSyntax());
+        getCmdMap().put(getName(), getDescription());
+        getSynMap().put(getName(), getSyntax());
     }
+
     public String getName() {
         return Time.class.getSimpleName().toLowerCase();
     }
+
     public String getDescription() {
         return "Returns the current time (german time)";
     }
+
     public String getSyntax() {
-        return "`" + Lib.PREFIX + getName() + "`";
+        return "`" + PREFIX + getName() + "`";
     }
 }
